06bd08b6b16c5d3eaa3a88c076e39f025e237da3
Robert Staddon <robertstaddon@gmail.com>
2025-06-27 14:52:30 -0500

    Update learndash_wc_credit_audit.php
    Final fixes and implementation of "Select a Product"

--- learndash_wc_credit_audit.php
+++ learndash_wc_credit_audit.php
@@ -3,7 +3,7 @@
  * Plugin Name: LearnDash WooCommerce Credit/Audit Purchase
  * Plugin URI: http://www.learndash.com
  * Description: Add two buttons to LearnDash courses to add credit/audit products to cart
- * Version: 2.7
+ * Version: 2.8
  * Author: Abundant Designs
  * Author URI: http://www.abundantdesigns.com
  * Text Domain: learndash_wc_credit_audit
@@ -47,11 +47,9 @@ class learndash_wc_credit_audit {
 
         if ( $settings_metabox_key === 'learndash-course-enrollment' ) {
 
-            error_log( var_export( $setting_option_fields, true ) );
-
             if (
                 class_exists( 'LearnDash_Settings_Metabox' ) &&
-                !empty( $settings_instance = LearnDash_Settings_Metabox::get_metabox_instance( 'LearnDash_Settings_Metabox_Course_Access_Settings' ) )
+                !empty( $settings_instance = LearnDash_Settings_Metabox::get_metabox_instance( 'LearnDash_Settings_Metabox_Course_Enrollment' ) )
             ) {
                 
                 // Retrieve existing values
@@ -66,31 +64,69 @@ class learndash_wc_credit_audit {
                     }
                 }
 
+                // Set up Product Select options
+                $select_product_options_default = array(
+                    '' => esc_html__( 'Select Product', 'learndash' ),
+                );
+
+                $select_product_options = $this->select_a_product();
+
+                if (
+                    is_array( $select_product_options )
+                    && ! empty( $select_product_options )
+                ) {
+                    $select_product_options = $select_product_options_default + $select_product_options;
+                } else {
+                    $select_product_options = $select_product_options_default;
+                }
+
+                $select_product_options_default = '';
+
                 // Load the sub_option_fields
                 $settings_sub_option_fields['course_price_type_wcca_fields'] = array(
                     'course_price_type_wcca_audit_button_product_id' => array(
-                        'name'    => 'course_price_type_wcca_audit_button_product_id',
-                        'label'     => esc_html__( 'Audit Button Product ID', 'learndash_wc_credit_audit' ),
-                        'type'    => 'text',
-                        'class'   => '-medium',
-                        'value'   => $setting_option_values['course_price_type_wcca_audit_button_product_id'],
-                        'help_text' => sprintf(
-                            esc_html_x( 'Enter the WooCommerce Product ID for the product that will need to be purchased to Audit this %s.', 'placeholder: course', 'learndash_wc_credit_audit' ),
-                            learndash_get_custom_label_lower( 'course' )
+                        'name'        => 'course_price_type_wcca_audit_button_product_id',
+                        'type'        => 'select',
+                        'label'       => esc_html__( 'Audit Button Product', 'learndash_wc_credit_audit' ),
+                        'default'     => '',
+                        'value'       => $setting_option_values['course_price_type_wcca_audit_button_product_id'],
+                        'options'     => $select_product_options,
+                        'placeholder' => $select_product_options_default,
+                        'help_text'   => sprintf(
+                            esc_html_x( 'Select the WooCommerce Product that will need to be purchased to Audit this %s.', 'placeholder: course', 'learndash_wc_credit_audit' ),
+                            learndash_get_custom_label( 'course' )
                         ),
-                        'default' => '',
+                        'rest'        => array(
+                            'show_in_rest' => LearnDash_REST_API::enabled(),
+                            'rest_args'    => array(
+                                'schema' => array(
+                                    'type'    => 'integer',
+                                    'default' => 0,
+                                ),
+                            ),
+                        ),
                     ),
                     'course_price_type_wcca_credit_button_product_id' => array(
-                        'name'      => 'course_price_type_wcca_credit_button_product_id',
-                        'label'     => esc_html__( 'Credit Button Product ID', 'learndash_wc_credit_audit' ),
-                        'type'      => 'text',
-                        'class'   => '-medium',
-                        'value'     => $setting_option_values['course_price_type_wcca_credit_button_product_id'],
-                        'help_text' => sprintf(
-                            esc_html_x( 'Enter the WooCommerce Product ID for the product that will need to be purchased to Credit this %s.', 'placeholder: course', 'learndash_wc_credit_audit' ),
-                            learndash_get_custom_label_lower( 'course' )
+                        'name'        => 'course_price_type_wcca_credit_button_product_id',
+                        'type'        => 'select',
+                        'label'       => esc_html__( 'Credit Button Product', 'learndash_wc_credit_audit' ),
+                        'default'     => '',
+                        'value'       => $setting_option_values['course_price_type_wcca_credit_button_product_id'],
+                        'options'     => $select_product_options,
+                        'placeholder' => $select_product_options_default,
+                        'help_text'   => sprintf(
+                            esc_html_x( 'Select the WooCommerce Product that will need to be purchased to Credit this %s.', 'placeholder: course', 'learndash_wc_credit_audit' ),
+                            learndash_get_custom_label( 'course' )
                         ),
-                        'default'   => '',
+                        'rest'        => array(
+                            'show_in_rest' => LearnDash_REST_API::enabled(),
+                            'rest_args'    => array(
+                                'schema' => array(
+                                    'type'    => 'integer',
+                                    'default' => 0,
+                                ),
+                            ),
+                        ),
                     ),
                 );
                 
@@ -119,11 +155,11 @@ class learndash_wc_credit_audit {
     
     /**
      * LearnDash save settings fields
-     * Hooked from class-ld-settings-metabox-course-access-settings.php
+     * Hooked from class-ld-settings-metabox-course-enrollment-settings.php
      */
     public function get_post_settings_field_updates( $settings_field_updates, $settings_metabox_key, $settings_screen_id ) {
 
-        if ( $settings_metabox_key === 'learndash-course-access-settings' ) {
+        if ( $settings_metabox_key === 'learndash-course-enrollment' ) {
             $post_values = $_POST[ $settings_metabox_key ];
 
             foreach( $this->settings_fields as $setting_field ) {
@@ -138,15 +174,16 @@ class learndash_wc_credit_audit {
         }
 
         return $settings_field_updates;
+        
     }
 
     /**
      * Final filter for LearnDash save values
-     * Hooked from class-ld-settings-metabox-course-access-settings.php
+     * Hooked from class-ld-settings-metabox-course-enrollment-settings.php
      */
     public function filter_saved_fields( $settings_values, $settings_metabox_key ) {
 
-        if ( $settings_metabox_key === 'learndash-course-access-settings' ) {
+        if ( $settings_metabox_key === 'learndash-course-enrollment' ) {
             // Overwrite "course_price" if using "wcca" price type - important for LearnDash Course Grid "course_list_template.php" ribbon display    
             if ( 'wcca' === $settings_values['course_price_type'] ) {
                 $settings_values['course_price'] = $this->get_price_display( $_POST['post_ID'] );
@@ -274,5 +311,28 @@ class learndash_wc_credit_audit {
         return $price_display;
     }
 
+    /**
+     * Helper function to return product list
+     */
+    public function select_a_product( $current_post_type = null ) {
+        $opt = array(
+            'post_type'   => 'product',
+            'post_status' => 'any',
+            'numberposts' => -1,
+            'orderby'     => 'title',
+            'order'       => 'ASC',
+        );
+
+        $posts      = get_posts( $opt );
+        $post_array = array();
+
+        if ( ! empty( $posts ) ) {
+            foreach ( $posts as $p ) {
+                $post_array[ $p->ID ] = $p->post_title;
+            }
+        }
+
+        return $post_array;
+    }
 }
 new learndash_wc_credit_audit();
